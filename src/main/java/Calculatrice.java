9cc165582657cccc6859905f78bfdfe2c2407b52
Esperance <chaisette@yahoo.fr>
2017-12-22 19:45:00 +0100

    Tests calculatrice passés avec succés

--- src/main/java/Calculatrice.java
+++ src/main/java/Calculatrice.java
@@ -1,2 +1,16 @@
 public class Calculatrice {
+
+    public static int addition(int a, int b){
+        return a + b ;
+    }
+
+    public static int soustraction(int a, int b){
+        return a - b;
+    }
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+    public static int division(int a, int b){
+        return a / b;
+    }
 }
--- src/test/java/CalculatriceTest.java
+++ src/test/java/CalculatriceTest.java
@@ -1,5 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CalculatriceTest {
 
+    @Test
+    public void addition() {
+        Assert.assertEquals(Calculatrice.addition(1 , 2), 3);
+    }
+
+    @Test
+    public void soustraction() {
+        Assert.assertEquals(Calculatrice.soustraction(4, 2), 2);
+    }
+
+    @Test
+    public void multiplication() {
+        Assert.assertEquals(Calculatrice.multiplication(2, 2), 4);
+    }
+
+    @Test
+    public void division() {
+        Assert.assertEquals(Calculatrice.division(5, 2), 2);
+    }
 }
